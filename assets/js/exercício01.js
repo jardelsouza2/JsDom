f6d73af4a7b7920eb46812f9767a4f4d7e4fa5e4
jardelsouza2 <55919894+jardelsouza2@users.noreply.github.com>
2019-10-17 21:49:22 -0300

    17/10

--- assets/js/exercício01.js
+++ assets/js/exercício01.js
@@ -1,14 +1,13 @@
 
-function simuladorSalario (){
+    function simuladorSalario (){
     const SALARIO_COMERCIAL = 1300;
     const PERCENTUAL_COMISSAO = 0.1;
-    const META_VENDA = 1000;
+    const META_VENDA = 1000; 
 
 
     var nomeVendedor = document.getElementById('nomeVendedor').value; //captura a valor (nome) digitado //
     var totalVendas = document.getElementById('totalVendas').value;
     
-
     if(totalVendas >= META_VENDA){
         var salarioBruto = SALARIO_COMERCIAL + (totalVendas * PERCENTUAL_COMISSAO);
         var comissao = (totalVendas * PERCENTUAL_COMISSAO);
@@ -26,13 +25,13 @@ function simuladorSalario (){
     console.log('Total de vendas: ' + totalVendas);
     console.log('salario bruto: ' + salarioBruto);
 }
-function exibirMensagemResultado(nome, salario, comissao){
-    let html = '<div class="col-sm-12 col-md-9 col-lg-9 pt-30">' + 
-                     '<div class="alert alert-success" role="alert">' + 
-                        '<h4>' + nome + ', seu salario bruto será de R$: ' + salario +'reias.<br> Voce recebou R$' + comissao + 'reais de comissao.</h4>' + 
-                    '</div>'    +
-                '</div>';
-                var mensagemResultado = document.getElementById('mensagemResultado');
-                    mensagemSucesso.className = 'row visible'; 
-                    mensagemResultado.innerhtml = html;
+    function exibirMensagemResultado(nome, salario, comissao){
+        let html = '<div class="col-sm-12 col-md-9 col-lg-9 pt-30">' + 
+                        '<div class="alert alert-success" role="alert">' + 
+                            '<h4>' + nome + ', seu salario bruto será de R$: ' + salario +'reias.<br> Voce recebou R$' + comissao + 'reais de comissao.</h4>' + 
+                        '</div>'    +
+                    '</div>';
+                    var mensagemResultado = document.getElementById('mensagemResultado');
+                        mensagemSucesso.className = 'row visible'; 
+                        mensagemResultado.innerhtml = html;
 }
